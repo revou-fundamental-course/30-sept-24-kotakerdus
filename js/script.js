194fe1c8383411b683a485d1682086f71a5788b8
kotakerdus <testonly27@gmail.com>
2024-10-10 14:19:05 +0800

    - Rename leftDropdownValue variable into leftDropVal also for the right side - Added more comment - Code cleanup

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const leftInput = document.getElementById("left-input");
 const rightInput = document.getElementById("right-input");
-const leftDropdown = document.getElementById("left-select");
-const rightDropdown = document.getElementById("right-select");
-let leftDropdownValue = leftDropdown.value; // Only for value reference
-let rightDropdownValue = rightDropdown.value;
+const leftDrop = document.getElementById("left-select");
+const rightDrop = document.getElementById("right-select");
+let leftDropVal = leftDrop.value; // Only for value reference
+let rightDropdVal = rightDrop.value;
 
 const formulaText = document.getElementById("formula-text");
 
@@ -20,8 +20,8 @@ function initValue() {
 }
 
 // Run these code if user done selecting from dropdown menu
-leftDropdown.onchange = (e) => { doneLeftSelecting(); }
-rightDropdown.onchange = (e) => { doneRightSelecting(); }
+leftDrop.onchange = (e) => { doneLeftSelecting(); }
+rightDrop.onchange = (e) => { doneRightSelecting(); }
 
 // 'onclick' run once as soon as the user clicked the input-box which select all the its content
 leftInput.onclick = (e) => { leftInput.select(); }
@@ -40,9 +40,11 @@ rightInput.oninput = (e) => {
 
 function inputWarning(whichInput) {
     if (whichInput.value == "") {
+        // Make the input-box turns yellow if error occured (input.value turns blank if invalid)
         leftInput.style.backgroundColor = 'yellow';
         rightInput.style.backgroundColor = 'yellow';
     } else {
+        // Return normal as soon as the value is valid
         leftInput.style.backgroundColor = '';
         rightInput.style.backgroundColor = '';
     }
@@ -52,17 +54,15 @@ function inputWarning(whichInput) {
 leftInput.onchange = (e) => {
     // If the input-box is blank (deleted all the number) or there's invalid string put 0 instead & reset the input warning color
     if (leftInput.value == "") leftInput.value = 0;
-    leftInput.style.backgroundColor = '';
-    rightInput.style.backgroundColor = '';
     leftInput.value -= 0; // Remove leading zero
+    inputWarning(leftInput); // Reset input-box background color
     calculateRight();
 }
 
 rightInput.onchange = (e) => {
     if (rightInput.value == "") rightInput.value = 0;
-    leftInput.style.backgroundColor = '';
-    rightInput.style.backgroundColor = '';
     rightInput.value -= 0;
+    inputWarning(rightInput);
     calculateLeft();
 }
 
@@ -71,29 +71,29 @@ function updateFormula() {
     let left = leftInput.value;
     let right = rightInput.value;
 
-    if      (leftDropdownValue == 0 && rightDropdownValue == 1) formulaText.innerHTML = "(" + left + "<b>°C</b> x <sup>9</sup>&frasl;<sub>5</sub>) + 32 = "          + right + "<b>°F</b>";
-    else if (leftDropdownValue == 0 && rightDropdownValue == 2) formulaText.innerHTML =       left + "<b>°C</b> + 273.15 = "                                         + right + "<b>°K</b>";
-    else if (leftDropdownValue == 1 && rightDropdownValue == 0) formulaText.innerHTML = "(" + left + "<b>°F</b> - 32) x <sup>5</sup>&frasl;<sub>9</sub> = "          + right + "<b>°C</b>";
-    else if (leftDropdownValue == 1 && rightDropdownValue == 2) formulaText.innerHTML = "(" + left + "<b>°F</b> - 32) x <sup>5</sup>&frasl;<sub>9</sub> + 273.15 = " + right + "<b>°K</b>";
-    else if (leftDropdownValue == 2 && rightDropdownValue == 0) formulaText.innerHTML =       left + "<b>°K</b> - 273.15 = "                                         + right + "<b>°C</b>";
-    else if (leftDropdownValue == 2 && rightDropdownValue == 1) formulaText.innerHTML = "(" + left + "<b>°K</b> - 273.15) x <sup>9</sup>&frasl;<sub>5</sub> + 32 = " + right + "<b>°F</b>";
+    if      (leftDropVal == 0 && rightDropdVal == 1) formulaText.innerHTML = "(" + left + "<b>°C</b> x <sup>9</sup>&frasl;<sub>5</sub>) + 32 = "          + right + "<b>°F</b>";
+    else if (leftDropVal == 0 && rightDropdVal == 2) formulaText.innerHTML =       left + "<b>°C</b> + 273.15 = "                                         + right + "<b>°K</b>";
+    else if (leftDropVal == 1 && rightDropdVal == 0) formulaText.innerHTML = "(" + left + "<b>°F</b> - 32) x <sup>5</sup>&frasl;<sub>9</sub> = "          + right + "<b>°C</b>";
+    else if (leftDropVal == 1 && rightDropdVal == 2) formulaText.innerHTML = "(" + left + "<b>°F</b> - 32) x <sup>5</sup>&frasl;<sub>9</sub> + 273.15 = " + right + "<b>°K</b>";
+    else if (leftDropVal == 2 && rightDropdVal == 0) formulaText.innerHTML =       left + "<b>°K</b> - 273.15 = "                                         + right + "<b>°C</b>";
+    else if (leftDropVal == 2 && rightDropdVal == 1) formulaText.innerHTML = "(" + left + "<b>°K</b> - 273.15) x <sup>9</sup>&frasl;<sub>5</sub> + 32 = " + right + "<b>°F</b>";
 }
 
 function doneLeftSelecting() {
     // Check if the selected dropdown is the same with right dropdown then swap between two
-    if (rightDropdownValue == leftDropdown.value) {
-        rightDropdown.selectedIndex = leftDropdownValue;
-        rightDropdownValue = rightDropdown.value;
+    if (rightDropdVal == leftDrop.value) {
+        rightDrop.selectedIndex = leftDropVal;
+        rightDropdVal = rightDrop.value;
         // Also swap between the input-boxes value
         let tempVal = leftInput.value;
         leftInput.value = rightInput.value;
         rightInput.value = tempVal;
 
-        leftDropdownValue = leftDropdown.value;
+        leftDropVal = leftDrop.value;
     } else {
         // If not the same then only calculate on the one that is changed
         // (so that you can convert like this = 100C -> 212F then -> 373.2K)
-        leftDropdownValue = leftDropdown.value;
+        leftDropVal = leftDrop.value;
         calculateLeft();
     }
 
@@ -101,16 +101,16 @@ function doneLeftSelecting() {
 }
 
 function doneRightSelecting() {
-    if (leftDropdownValue == rightDropdown.value) {
-        leftDropdown.selectedIndex = rightDropdownValue;
-        leftDropdownValue = leftDropdown.value;
+    if (leftDropVal == rightDrop.value) {
+        leftDrop.selectedIndex = rightDropdVal;
+        leftDropVal = leftDrop.value;
         let tempVal = rightInput.value;
         rightInput.value = leftInput.value;
         leftInput.value = tempVal;
 
-        rightDropdownValue = rightDropdown.value;
+        rightDropdVal = rightDrop.value;
     } else {
-        rightDropdownValue = rightDropdown.value;
+        rightDropdVal = rightDrop.value;
         calculateRight();
     }
 
@@ -122,12 +122,12 @@ function calculateRight() {
     let leftValue = parseFloat(leftInput.value);
     let rightValue = parseFloat(rightInput.value);
 
-    if      (leftDropdownValue == 0 && rightDropdownValue == 1) rightValue = (leftValue * (9/5))   + 32;
-    else if (leftDropdownValue == 0 && rightDropdownValue == 2) rightValue = leftValue + 273.15;
-    else if (leftDropdownValue == 1 && rightDropdownValue == 0) rightValue = (leftValue - 32)      * (5/9);
-    else if (leftDropdownValue == 1 && rightDropdownValue == 2) rightValue = ((leftValue - 32)     * (5/9)) + 273.15;
-    else if (leftDropdownValue == 2 && rightDropdownValue == 0) rightValue = leftValue - 273.15;
-    else if (leftDropdownValue == 2 && rightDropdownValue == 1) rightValue = ((leftValue - 273.15) * (9/5)) + 32;
+    if      (leftDropVal == 0 && rightDropdVal == 1) rightValue = (leftValue * (9/5))   + 32;
+    else if (leftDropVal == 0 && rightDropdVal == 2) rightValue = leftValue + 273.15;
+    else if (leftDropVal == 1 && rightDropdVal == 0) rightValue = (leftValue - 32)      * (5/9);
+    else if (leftDropVal == 1 && rightDropdVal == 2) rightValue = ((leftValue - 32)     * (5/9)) + 273.15;
+    else if (leftDropVal == 2 && rightDropdVal == 0) rightValue = leftValue - 273.15;
+    else if (leftDropVal == 2 && rightDropdVal == 1) rightValue = ((leftValue - 273.15) * (9/5)) + 32;
 
     rightInput.value = leftInput.value == "" ? "" : Math.round(rightValue * 100) / 100; // Blank to avoid errors & show decimals if needed
     updateFormula();
@@ -137,12 +137,12 @@ function calculateLeft() {
     let leftValue = parseFloat(leftInput.value);
     let rightValue = parseFloat(rightInput.value);
 
-    if      (leftDropdownValue == 0 && rightDropdownValue == 1) leftValue = (rightValue - 32)      * (5/9);
-    else if (leftDropdownValue == 0 && rightDropdownValue == 2) leftValue = rightValue - 273.15;
-    else if (leftDropdownValue == 1 && rightDropdownValue == 0) leftValue = (rightValue * (9/5))   + 32;
-    else if (leftDropdownValue == 1 && rightDropdownValue == 2) leftValue = ((rightValue - 273.15) * (9/5)) + 32;
-    else if (leftDropdownValue == 2 && rightDropdownValue == 0) leftValue = rightValue + 273.15;
-    else if (leftDropdownValue == 2 && rightDropdownValue == 1) leftValue = ((rightValue - 32)     * (5/9)) + 273.15;
+    if      (leftDropVal == 0 && rightDropdVal == 1) leftValue = (rightValue - 32)      * (5/9);
+    else if (leftDropVal == 0 && rightDropdVal == 2) leftValue = rightValue - 273.15;
+    else if (leftDropVal == 1 && rightDropdVal == 0) leftValue = (rightValue * (9/5))   + 32;
+    else if (leftDropVal == 1 && rightDropdVal == 2) leftValue = ((rightValue - 273.15) * (9/5)) + 32;
+    else if (leftDropVal == 2 && rightDropdVal == 0) leftValue = rightValue + 273.15;
+    else if (leftDropVal == 2 && rightDropdVal == 1) leftValue = ((rightValue - 32)     * (5/9)) + 273.15;
 
     leftInput.value = rightInput.value == "" ? "" : Math.round(leftValue * 100) / 100;
     updateFormula();
